707ed94f381db41453b336e48d99ef31f5824e2b
Alejandro Rodriguez <ardzoht@gmail.com>
2016-03-25 21:18:01 -0700

    adding files

---
+++ CapaSoftware/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CapaSoftware.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CapaSoftware/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapaSoftware.Startup))]
+namespace CapaSoftware
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
